11aa4e8eab58c3428ac037d052b522e8e2d9284b
elisafvic <elisapersea@gmail.com>
2024-01-18 12:24:29 +0100

    Rate model tests

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,2 +1,4 @@
 class Rate < ApplicationRecord
+  validates :rate, :annual_expenses, :hours_day, :hours_year, :billable_percent, :net_month, :tax_percent, :gross_year, presence: true
+  validates :rate, :annual_expenses, :hours_day, :hours_year, :billable_percent, :net_month, :tax_percent, :gross_year, numericality: true
 end
--- test/models/rate_test.rb
+++ test/models/rate_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class RateTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "rate attributes must not be empty" do
+    rate = Rate.new
+    assert rate.invalid?
+    assert rate.errors[:rate].any?
+    assert rate.errors[:annual_expenses].any?
+    assert rate.errors[:hours_day].any?
+    assert rate.errors[:hours_year].any?
+    assert rate.errors[:billable_percent].any?
+    assert rate.errors[:net_month].any?
+    assert rate.errors[:tax_percent].any?
+    assert rate.errors[:gross_year].any?
+  end
+
+  test "rate attributes must be numeric" do
+    rate = Rate.new
+    rate.rate = 'abc'
+    rate.annual_expenses = 'abc'
+    rate.hours_day = 'abc'
+    rate.hours_year = 'abc'
+    rate.billable_percent = 'abc'
+    rate.net_month = 'abc'
+    rate.tax_percent = 'abc'
+    rate.gross_year = 'abc'
+
+    assert rate.invalid?
+    assert_equal ["is not a number"], rate.errors[:rate]
+    assert_equal ["is not a number"], rate.errors[:annual_expenses]
+    assert_equal ["is not a number"], rate.errors[:hours_day]
+    assert_equal ["is not a number"], rate.errors[:hours_year]
+    assert_equal ["is not a number"], rate.errors[:billable_percent]
+    assert_equal ["is not a number"], rate.errors[:net_month]
+    assert_equal ["is not a number"], rate.errors[:tax_percent]
+    assert_equal ["is not a number"], rate.errors[:gross_year]
   end
 end
