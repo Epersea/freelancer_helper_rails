314ac132ace339b208dc169e5f87cae7acca6ff4
elisafvic <elisapersea@gmail.com>
2024-01-18 17:57:13 +0100

    Refactor Rate controller

--- app/controllers/rate_controller.rb
+++ app/controllers/rate_controller.rb
@@ -1,9 +1,5 @@
 class RateController < ApplicationController
-  def index
-  end
-  
-  def new
-  end
+  before_action :find_rate, except: [:create, :index, :new]
 
   def create
     @rate = Rate.new
@@ -13,33 +9,31 @@ class RateController < ApplicationController
     redirect_to show_rate_path(@rate)
   end
 
-  def show
-    @rate = Rate.find(params[:id])
-  end
+  # def show
+  # end
 
-  def edit
-    @rate = Rate.find(params[:id])
-  end
+  # def edit
+  # end
 
   def update
-    rate_id = params[:id]
-    @rate = Rate.find(rate_id)
-
     add_calculations_to_rate(@rate, params)
 
     redirect_to show_rate_path(@rate)
   end
 
   def destroy
-    rate_id = params[:id]
-    rate = Rate.find(rate_id)
-    
-    rate.destroy
+    @rate.destroy
 
     redirect_to root_path
   end
 
   private
+
+  def find_rate
+    @rate = Rate.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+  end
 
   def add_calculations_to_rate(rate, user_info)
     rate_calculator = RateCalculator.new(user_info)
