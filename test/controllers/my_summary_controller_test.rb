dfcc1fd15bc10ab0a6a610140be26a6904fd79eb
elisafvic <elisapersea@gmail.com>
2024-02-23 18:34:31 +0100

    Add My Summary test for users without rate

--- test/controllers/my_summary_controller_test.rb
+++ test/controllers/my_summary_controller_test.rb
@@ -2,21 +2,36 @@ require "test_helper"
 
 class MySummaryControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:darlene)
-    @rate = Rate.find_by(user_id: @user.id)
+    @user_with_rate = users(:darlene)
+    @rate = Rate.find_by(user_id: @user_with_rate.id)
+    @user_without_rate = users(:elliot)
   end
 
-  test "should show Rate to a logged in user" do
+  test "should show Rate to a logged in user with a rate" do
 
     post "/login", params: {
-      name: 'Darlene',
+      name: @user_with_rate.name,
       password: 'secret'
     }
 
     get "/my_summary"
 
-    assert_select 'h1', "#{@user.name}, this is your Freelancer Summary"
+    assert_select 'h1', "#{@user_with_rate.name}, this is your Freelancer Summary"
     assert_select 'h3', "Your minimum rate per hour should be #{@rate.rate}"
+  end
+
+  test "should show invitation to fill rate to a logged in user without a rate" do
+
+    post "/login", params: {
+      name: @user_without_rate.name,
+      password: 'secret'
+    }
+
+    get "/my_summary"
+
+    assert_select 'h1', "Freelancer Summary"
+    assert_select 'p', "Looks like you haven't provided any data about your freelancer journey. Our Rate Calculator is a great place to start!"
+
   end
 
   test "should show login prompt to a logged out user" do
