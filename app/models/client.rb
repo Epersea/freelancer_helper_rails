472a0acfdb37b2f71ff9c4b44ab98bd5424e5ea2
elisafvic <elisapersea@gmail.com>
2024-03-05 19:38:23 +0100

    Client model tests

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ApplicationRecord
   belongs_to :user
-  validates :name, :hours_worked, :amount_billed, presence: true
-  validates :hours_worked, :amount_billed, numericality: true
+  validates :name, :hours_worked, :amount_billed, :user_id, presence: true
+  validates :hours_worked, :amount_billed, numericality: { greater_than_or_equal_to: 0.5 }
 end
