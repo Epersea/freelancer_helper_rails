c4450f1faf70603ba0378f3deef33ac5a4069873
elisafvic <elisapersea@gmail.com>
2024-04-19 17:03:15 +0200

    Redirect user to login after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path, notice: "User #{@user.name} was successfully created"
+      redirect_to new_session_path, notice: "User #{@user.name} was successfully created"
     else
       render :new, status: :unprocessable_entity 
     end
